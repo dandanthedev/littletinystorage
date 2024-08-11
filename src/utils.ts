e0e9c8f4e48c7d10227e70c319caf2c414ef0cbc
Daan <20974756+daanschenkel@users.noreply.github.com>
2024-08-11 22:54:17 +0200

    Fix setting header after writing head

--- src/utils.ts
+++ src/utils.ts
@@ -35,9 +35,10 @@ export function resp(
     if (type === "xml") res.setHeader("Content-Type", "text/xml");
   }
 
+  if (!type) res.setHeader("Content-Type", "text/plain");
+
   if (status) res.writeHead(status);
 
-  if (!type) res.setHeader("Content-Type", "text/plain");
   if (typeof body === "string") {
     res.end(body);
   } else if (body && "pipe" in body && typeof body.pipe === "function") {
