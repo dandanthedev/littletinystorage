96f41b43f6f9855e2251ba6dade8d6badcae7d46
Daan <20974756+daanschenkel@users.noreply.github.com>
2024-08-12 14:14:37 +0200

    return token after generating

--- src/api/generateToken.ts
+++ src/api/generateToken.ts
@@ -19,10 +19,17 @@ export default async function handle(
   if (type && ["upload", "download", "delete", "rename"].indexOf(type) === -1)
     return resp(res, 400, "Invalid type");
 
-  if(downloadAs && type && type !== "download") return resp(res, 400, "Downloadas can only be used on a download type")
+  if (downloadAs && type && type !== "download")
+    return resp(res, 400, "Downloadas can only be used on a download type");
 
-  const token = await generateSignedToken(bucket, file, type, expiresIn, downloadAs);
+  const token = await generateSignedToken(
+    bucket,
+    file,
+    type,
+    expiresIn,
+    downloadAs
+  );
   if (!token) return resp(res, 400, "Error generating token");
 
-  resp(res, 200, token);
+  return resp(res, 200, token);
 }
